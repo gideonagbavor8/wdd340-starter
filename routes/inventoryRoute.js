fe267f296820f14d919e697a4c6cfdbdf2264883
gideonagbavor8 <gagbavor@byupathway.edu>
2025-07-19 21:47:04 +0000

    Week 03 Learning Activities: 1. Content Delivery and the MVC Pattern 2. Deployment and Testing 3. Error Handling

---
+++ routes/inventoryRoute.js
@@ -0,0 +1,10 @@
+// Needed REsources
+const express = require("express")
+const router = new express.Router()
+const invController = require("../controllers/invController")
+const { route } = require("./static")
+
+// Route to build inventory by classification view
+router.get("/type/:classificationId", invController.buildByClassificationId);
+
+module.exports = router;
