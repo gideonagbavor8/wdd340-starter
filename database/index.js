30f249a1883d20053eb133143b1fb38420ec70e7
gideonagbavor8 <gagbavor@byupathway.edu>
2025-07-20 23:08:11 +0000

    1. Content Delivery and the MVC Pattern. 2. Deployment and Testing 3. Error Handling

--- database/index.js
+++ database/index.js
@@ -10,28 +10,28 @@ let pool
 if (process.env.NODE_ENV == "development") {
     pool = new Pool({
         connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false,
-        },
+        ssl: { rejectUnauthorized: false },
     })
 
-// Added for troubleshooting queries
-// during development
-module.exports = {
-    async query(text, params) {
-        try {
-            const res = await pool.query(text, params)
-            console.log("executed query", { text })
-            return res
-        } catch (error) {
-            console.error("error in query", { text })
-            throw error
-        }
-    },
-}
+
+    // Added for troubleshooting queries
+    // during development
+    module.exports = {
+        async query(text, params) {
+            try {
+                const res = await pool.query(text, params)
+                console.log("executed query", { text })
+                return res
+            } catch (error) {
+                console.error("error in query", { text })
+                throw error
+            }
+        },
+    }
 } else {
     pool = new Pool({
         connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
     })
     module.exports = pool
 }
